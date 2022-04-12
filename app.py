a34e37330658d88fc2dbfc7b9f1c9f5276b2c589
MD.MEJBAH UDDIN <79783055+captainmejbah@users.noreply.github.com>
2022-04-12 21:42:47 +0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, escape, request, render_template
+import pickle
+
+vactor = pickle.load(open("G:/ProjectFakeNews/vectorizer.pkl", 'rb'))
+model = pickle.load(open("G:/ProjectFakeNews/finalizen.model.pkl", 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/prediction', methods=['GET', 'POST'])
+def prediction():
+    if request.method == "POST":
+        news = str(request.form['news'])
+        print(news)
+
+        predict = model.predict(vactor.transform([news]))[0]
+        print(predict)
+
+        return render_template("prediction.html", prediction_text="News headline is -> {}".format(predict))
+
+
+    else:
+        return render_template("prediction.html")
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
